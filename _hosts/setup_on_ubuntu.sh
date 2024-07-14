48ca9873e1391cda6490d6b72d57df1a21d00209
scillidan <scillidan@yandex.com>
2024-07-14 16:45:04 +0800

    Update setup_on_ubuntu.sh

--- _hosts/setup_on_ubuntu.sh
+++ _hosts/setup_on_ubuntu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp "/etc/hosts" "/etc/hosts_bak"
-del "/etc/hosts"
-type "/etc/hosts_bak" "~/Github/SETUP-note/_hosts/hosts_for_ubuntu" > "/etc/hosts"
+rm "/etc/hosts"
+cat "/etc/hosts_bak" "~/Github/SETUP-note/_hosts/hosts_for_ubuntu" | sudo tee -a /etc/hosts
