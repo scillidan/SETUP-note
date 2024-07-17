7f7c945e2be7574250a977d83a3ceb165b8bc65b
scillidan <scillidan@yandex.com>
2024-07-17 19:22:17 +0800

    1

---
+++ irfanview/setup_on_windows.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem set HOME=C:\Users\User
+
+set IRFANVIEW_OPT=%HOME%\Opt\iview
+set IRFANVIEW_SETUP_NOTE=%HOME%\Github\SETUP-note\irfanview
+
+del "%IRFANVIEW_OPT%\i_view64.ini"
+mklink "%IRFANVIEW_OPT%\i_view64.ini" "%IRFANVIEW_SETUP_NOTE%\i_view64.ini"
+
+pause
