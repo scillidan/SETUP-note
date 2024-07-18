548b7677c13a5c29137fdf3782cfce8140959d99
scillidan <scillidan@yandex.com>
2024-07-18 19:14:22 +0800

    1

---
+++ ditto/setup_on_windows.bat
@@ -0,0 +1,11 @@
+@echo off
+
+rem set HOME=C:\Users\User
+
+set DITTO_OPT=%HOME%\Opt\Ditto
+set DITTO_SETUP_NOTE=%HOME%\Github\SETUP-note\ditto
+
+del "%DITTO_OPT%\Ditto.Settings"
+mklink "%DITTO_OPT%\Ditto.Settings" "%DITTO_SETUP_NOTE%\Ditto.Settings"
+
+pause
